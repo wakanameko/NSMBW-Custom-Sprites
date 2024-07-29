19c6d04367d747a99caa92c47b8eb57e2efaab8f
wakanameko <63937252+wakanameko@users.noreply.github.com>
2024-07-29 22:50:45 +0900

    Create wakanalib.h

---
+++ Libs/wakanalib.h
@@ -0,0 +1,24 @@
+#ifndef WAKANALIB_H
+#define WAKANALIB_H 
+/*VERSION 1.0 SMV*/
+#include <common.h>
+
+// increase rests
+extern "C" void addRest(int playerIDRest, int howMany, bool reallyAddRest);	// reallyAddRest is always true
+extern "C" void incRestAll(bool reallyAddRest);	// reallyAddRest is always true
+
+// stage funcs
+extern "C" void oneUPPopup(Vec pos);   // set the position (x,y)
+extern "C" void AddScore(int howMany, int color);
+extern "C" void DisplayScoreAddPopup(Vec pos, int valuemaybe, int playerIDuint, bool unkbool);  // DONT USE
+
+// zone settings
+extern "C" float zoneZoomFloat;    // Default is 2.0
+
+//
+extern "C" char aP_rcha_2;     //"P_rcha"
+
+// daEnDpakkunBase_c::
+extern "C" void pakkunYoshiFumiJumpSet(dEn_c *apThis, daPlBase_c *unk);    // 2nd argument is unknown
+
+#endif /* WAKANALIB_H */
