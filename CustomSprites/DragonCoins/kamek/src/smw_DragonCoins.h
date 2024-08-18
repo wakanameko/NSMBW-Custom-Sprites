e471883f9c7182b1ec414c532da2911af3c1753b
wakanameko <63937252+wakanameko@users.noreply.github.com>
2024-08-19 01:52:43 +0900

    Update smw_DragonCoins.h
    v1.0.0

--- CustomSprites/DragonCoins/kamek/src/smw_DragonCoins.h
+++ CustomSprites/DragonCoins/kamek/src/smw_DragonCoins.h
@@ -2,9 +2,15 @@
 #define SMW_DRAGONCOINS_H 
 
 #include <common.h>
+#include <game.h>
+#include <g3dhax.h>
+#include <sfx.h>
+#include <profile.h>
+#include <wakanalib.h>
 
 extern "C" int collectionDCoin = 0;
 extern "C" int collectedDCoinID[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 //extern "C" u8 numDCoinWorldANDLevelArr[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+extern "C" u8 currentLevelCashe = 0;
 
 #endif /* SMW_DRAGONCOINS_H */
