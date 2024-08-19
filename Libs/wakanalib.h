340df407266a96f252c08dff25ca63065f43fd78
wakanameko <63937252+wakanameko@users.noreply.github.com>
2024-08-19 21:00:42 +0900

    Update wakanalib.h
    Completed the DisplayScoreAddPopup

--- Libs/wakanalib.h
+++ Libs/wakanalib.h
@@ -10,7 +10,7 @@ extern "C" void incRestAll(bool reallyAddRest);	// reallyAddRest is always true
 // stage funcs
 extern "C" void oneUPPopup(Vec pos);   // set the position (x,y)
 extern "C" void AddScore(int howMany, int color);
-extern "C" void DisplayScoreAddPopup(Vec pos, int valuemaybe, int playerIDuint, bool unkbool);  // DONT USE
+extern "C" void DisplayScoreAddPopup(Vec pos, int smallScoreID, int playerID, bool wantToShowThisForever);  // smallScoreID list is here -> https://note.com/reaz0n23/n/nb504e036da6d#143de39b-221f-4909-94f8-cc11e4dbdfdd
 
 // zone settings
 extern "C" float zoneZoomFloat;    // Default is 2.0
